b828f8cbf3a5a19a92f2d70447aa4b0990e4ac68
orzpainter <71520758+orzpainter@users.noreply.github.com>
2021-07-10 09:39:45 +0800

    Create IUniswapV2Factory.sol

---
+++ IUniswapV2Factory.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: Unlicensed
+pragma solidity ^0.8.4;
+
+interface IUniswapV2Factory {
+    event PairCreated(
+        address indexed token0,
+        address indexed token1,
+        address pair,
+        uint256
+    );
+
+    function feeTo() external view returns (address);
+
+    function feeToSetter() external view returns (address);
+
+    function getPair(address tokenA, address tokenB)
+        external
+        view
+        returns (address pair);
+
+    function allPairs(uint256) external view returns (address pair);
+
+    function allPairsLength() external view returns (uint256);
+
+    function createPair(address tokenA, address tokenB)
+        external
+        returns (address pair);
+
+    function setFeeTo(address) external;
+
+    function setFeeToSetter(address) external;
+}
+
+// 0xA8cf08d76b4c5A6d830396DAF912180E5396B422
+//
